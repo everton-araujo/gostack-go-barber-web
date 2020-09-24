e03f8e9b6c9d042e4902b989ecb076498dea0952
Everton Araujo <everton.aaraujo@outlook.com>
2020-09-23 23:25:22 -0300

    Showing validations errors to user

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
     isFocused: boolean;
     isFilled: boolean;
+    isError: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -19,6 +22,10 @@ export const Container = styled.div<ContainerProps>`
     & + div {
         margin-top: 8px;
     }
+
+    ${props => props.isError && css`
+        border-color: #C53030;
+    `}
 
     ${props => props.isFocused && css`
         color: #FF9000;
@@ -43,5 +50,23 @@ export const Container = styled.div<ContainerProps>`
 
     svg {
         margin-right: 16px;
+    }
+`;
+
+export const Error = styled(Tooltip)`
+    height: 20px;   
+    margin-left: 16px;
+
+    svg {
+        margin: 0;
+    }
+
+    span {
+        background: #C53030;
+        color: #FFF;
+
+        &::before {
+            border-color: #C53030 transparent;
+        }
     }
 `;
