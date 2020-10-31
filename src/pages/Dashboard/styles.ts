b34ede6b3e9924175db35600ea01f53017a1830f
Everton Araujo <everton.aaraujo@outlook.com>
2020-10-30 23:19:12 -0300

    Profile page created

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -54,8 +54,13 @@ export const Profile = styled.div`
             color: #F4EDE8;
         }
 
-        strong {
+        a {
+            text-decoration: none;
             color: #FF9000;
+
+            &:hover {
+                opacity: 0.8;
+            }
         }
     }
 `;
